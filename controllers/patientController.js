8723215eb58b6b3baec8a5720b214a18e7309db3
AdiSyahrul <adisyahrul27@gmail.com>
2024-02-19 01:23:24 +0700

    fix tambahPatient

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -2,7 +2,7 @@ const Patient = require('../models/patientModel.js');
 const User = require('../models/userModel.js');
 
 async function tambahPatient(req, res) {
-  const { nama, umur, jenisPenyakit, catatan, kode, alamatRumah, alamatTujuan } = req.body;
+  let { nama, umur, jenisPenyakit, catatan, kode, alamatRumah, alamatTujuan } = req.body;
   if (!nama || !umur || !jenisPenyakit || !catatan || !kode || !alamatRumah || !alamatTujuan) {
     return res.status(400).json({
       status: 400,
@@ -12,6 +12,19 @@ async function tambahPatient(req, res) {
   }
 
   try {
+    if (typeof alamatRumah === 'string') alamatRumah = JSON.parse(alamatRumah);
+    if (typeof alamatTujuan === 'string') alamatTujuan = JSON.parse(alamatTujuan);
+    alamatRumah = {
+      name: alamatRumah.name,
+      longi: alamatRumah.longi || alamatRumah.longitude,
+      lat: alamatRumah.lat || alamatRumah.latitude
+    };
+
+    alamatTujuan = {
+      name: alamatTujuan.name,
+      longi: alamatTujuan.longi || alamatTujuan.longitude,
+      lat: alamatTujuan.lat || alamatTujuan.latitude
+    };
     const existingKode = await Patient.findOne({ kode });
     if (existingKode) {
       return res.status(400).json({
